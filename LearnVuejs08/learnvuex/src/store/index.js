f878991d081880e620464402c1c71b877ecc655b
hatana <2397663712@qq.com>
2023-12-12 20:07:22 +0800

    p139

---
+++ LearnVuejs08/learnvuex/src/store/index.js
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { INCREMENT } from './mutations-types'
+
+// 1.安装插件
+Vue.use(Vuex)
+
+// 2.创建对象
+const store = new Vuex.Store({
+    state: {
+        counter: 1000,
+        students: [
+            {id: 110, name: 'why', age: 18},
+            {id: 111, name: 'kobe', age: 24},
+            {id: 112, name: 'james', age: 30},
+            {id: 113, name: 'curry', age: 10},
+        ],
+        info: {
+            name: 'kobe',
+            age: 40,
+            height: 1.98
+        }
+    },
+    mutations: {
+        // 方法
+        [INCREMENT](state){
+            state.counter++
+        },
+        decrement(state){
+            state.counter--
+        },
+        incrementCount(state,payload) {
+            // console.log(count)
+            state.counter += payload.count
+        },
+        addStudent(state,stu) {
+            state.students.push(stu)
+        },
+        updateInfo(state){
+            // state.info.name = 'coderwhy'
+            // state.info['address'] = '洛杉矶'
+            // Vue.set(state.info, 'address','洛杉矶')
+            // 该方法做不到响应式
+            // delete state.info.age
+            Vue.delete(state.info, 'age')
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+        powerCounter(state){
+            return state.counter * state.counter
+        },
+        more20stu(state) {
+            return state.students.filter(s => s.age > 20)
+        },
+        more20stuLength(state,getters) {
+            return getters.more20stu.length
+        },
+        moreAgeStu(state) {
+            // return function (age) {
+            //     return state.students.filter(s => s.age > age)
+            // }
+            return age => {
+                return state.students.filter(s => s.age > age)
+            }
+        }
+    },
+    modules: {
+
+    }
+})
+
+// 3.导出store对象
+export default store

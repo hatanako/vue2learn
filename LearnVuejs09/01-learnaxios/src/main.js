7991ba818b990433cb04dbff851a52706bbaaea4
hatana <2397663712@qq.com>
2023-12-19 20:04:04 +0800

    p160

--- LearnVuejs09/01-learnaxios/src/main.js
+++ LearnVuejs09/01-learnaxios/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 
 import axios from 'axios'
+import { request } from './network/request'
 
 Vue.config.productionTip = false
 
@@ -67,29 +68,59 @@ new Vue({
 // })
 
 // 4.创建对应的axios的实例
-const instance1 = axios.create({
-  baseURL: 'http://123.207.32.32:8000',
-  timeout: 5000
-})
+// const instance1 = axios.create({
+//   baseURL: 'http://123.207.32.32:8000',
+//   timeout: 5000
+// })
 
-instance1({
+// instance1({
+//   url: '/home/multidata'
+// }).then(res =>{
+//   console.log(res);
+// })
+
+// instance1({
+//   url: '/home/data',
+//   params: {
+//     type: 'pop',
+//     page: 1
+//   }
+// }).then(res => {
+//   console.log(res);
+// })
+
+// const instance2 = axios.create({
+//   baseURL: 'http://222.111.33.33:8000',
+//   timeout: 10000,
+//   // headers: {}
+// })
+
+import { request } from './network/request'
+
+// request({
+//   url: '/home/multidata'
+// }, res => {
+//   console.log(res);
+// }, err => {
+//   console.log(err);
+// })
+
+// request({
+//   baseConfig: {
+
+//   },
+//   success: function(res){
+
+//   },
+//   failure: function(err){
+
+//   }
+// })
+
+request({
   url: '/home/multidata'
-}).then(res =>{
-  console.log(res);
-})
-
-instance1({
-  url: '/home/data',
-  params: {
-    type: 'pop',
-    page: 1
-  }
 }).then(res => {
   console.log(res);
-})
-
-const instance2 = axios.create({
-  baseURL: 'http://222.111.33.33:8000',
-  timeout: 10000,
-  // headers: {}
+}).catch(err => {
+  console.log(err);
 })

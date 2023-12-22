81822c351773319c3e5faeb10a12bed258f5fc7b
hatana <2397663712@qq.com>
2023-12-22 19:43:17 +0800

    p181

---
+++ LearnVuejs09/supermall/src/views/detail/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="detail">
+
+    </div>
+</template>
+
+<script>
+import DetailNavBar from './childComps/DetailNavBar.vue'
+
+export default {
+ name:"Detail",
+ components: {
+  DetailNavBar
+ },
+ data() {
+    return {
+        iid: null
+    }
+ },
+ created() {
+    this.iid = this.$route.params.iid
+
+ }
+}
+</script>
+
+<style>
+
+</style>
